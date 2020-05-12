8a835f7e24a40e598eeb06e3739a01fa195135d7
Haider Khan <haiderkhan.live@gmail.com>
2020-05-13 04:37:20 +0500

    added golang variables examples

--- examples/values/values.go
+++ examples/values/values.go
@@ -8,10 +8,14 @@ func main() {
 
 	fmt.Println("Values Examples")
 
+	//Strings, which can be added together with +.
 	fmt.Println("I'am" + " Haider Khan") //I'am Haider Khan
-	fmt.Println("1+1=", 1+1)             //1+1= 2
-	fmt.Println("7.0/3.0", 7.0/3.0)      //7.0/3.0 2.3333333333333335
 
+	//Integers and floats.
+	fmt.Println("1+1=", 1+1)        //1+1= 2
+	fmt.Println("7.0/3.0", 7.0/3.0) //7.0/3.0 2.3333333333333335
+
+	//Booleans, with boolean operators as you’d expect.
 	fmt.Println(true && false) //false
 	fmt.Println(true || false) //true
 	fmt.Println(!true)         //false
