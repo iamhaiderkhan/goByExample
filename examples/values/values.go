809b2851e465e1a1d79fe0e079ffb62f9af797ef
Haider Khan <haiderkhan.live@gmail.com>
2020-05-13 03:44:33 +0500

    adding printed values

--- examples/values/values.go
+++ examples/values/values.go
@@ -8,11 +8,11 @@ func main() {
 
 	fmt.Println("Values Examples")
 
-	fmt.Println("I'am" + "\nHaider Khan")
-	fmt.Println("1+1=", 1+1)
-	fmt.Println("7.0/3.0", 7.0/3.0)
+	fmt.Println("I'am" + " Haider Khan") //I'am Haider Khan
+	fmt.Println("1+1=", 1+1)             //1+1= 2
+	fmt.Println("7.0/3.0", 7.0/3.0)      //7.0/3.0 2.3333333333333335
 
-	fmt.Println(true && false)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	fmt.Println(true && false) //false
+	fmt.Println(true || false) //true
+	fmt.Println(!true)         //false
 }
